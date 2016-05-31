e64fae9d7d725a1439c104b3304f684ab436f669
jenmcphail <mcphail.jen@gmail.com>
2016-05-31 16:35:25 -0700

    progress on case statement

--- wip/deaf_grandma.rb
+++ wip/deaf_grandma.rb
@@ -7,16 +7,23 @@ question_count = 0
 puts "Grandma: HELLO, DEAR"
 
 loop do
-question = gets.chomp
-question_count +=1
+	question = gets.chomp
+	question_count +=1
 
-#if question is not all caps
-puts "WHAT'S THAT, SONNY?"
-#if question is all caps
-puts "NO DEAR, NOT SINCE 1928."
+	case
+	when # question is not all caps
+		puts "WHAT'S THAT, SONNY?"
+		break
+	when #question is all caps
+		puts "NO DEAR, NOT SINCE 1928."
+		break
+	next
+	end
 
-if question_count %5 == 0
-	puts "Grandma: It's getting late, dear."
-elsif question_count >= 20 && question != "GOODBYE GRANDMA"
-	puts "Grandma: ZZZZZZZZZZZ"
+	if question_count %5 == 0
+		puts "Grandma: It's getting late, dear."
+	elsif question_count >= 20 && question != "GOODBYE GRANDMA"
+		puts "Grandma: ZZZZZZZZZZZ"
+	end
+
 end
