a3c5c3cfdebf4686c51a226e98ce85e02d7d5b45
jenmcphail <mcphail.jen@gmail.com>
2016-05-31 16:24:02 -0700

    initial commit

---
+++ wip/deaf_grandma.rb
@@ -0,0 +1,22 @@
+question_count = 0
+
+# Program runs -> greeted by grandma, prompts user for input
+#If user input not all caps, grandma responds: "WHAT'S THAT SONNY?"
+#If user input all caps, grandma responds: "NO DEAR, NOT SINCE 1928."
+
+puts "Grandma: HELLO, DEAR"
+
+loop do
+question = gets.chomp
+question_count +=1
+
+#if question is not all caps
+puts "WHAT'S THAT, SONNY?"
+#if question is all caps
+puts "NO DEAR, NOT SINCE 1928."
+
+if question_count %5 == 0
+	puts "Grandma: It's getting late, dear."
+elsif question_count >= 20 && question != "GOODBYE GRANDMA"
+	puts "Grandma: ZZZZZZZZZZZ"
+end
